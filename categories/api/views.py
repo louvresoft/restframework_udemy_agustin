e1702b461fca75cc9ca004ce87b370b0cd9f32a7
electroefos <losefosmx@gmail.com>
2021-08-14 21:30:53 -0500

    cambio de busqueda de id a slug con el campo lookup_field

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
 
 class CategoryApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'slug'
+    # busca por propiedad slug en vez que por id
