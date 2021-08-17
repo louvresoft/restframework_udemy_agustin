d1180ef4b12184f0876f0158ef202ae3c520446a
electroefos <losefosmx@gmail.com>
2021-08-17 12:26:39 -0500

    serializando proyectos

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+
+from categories.api.serializers import CategorySerializer
 from posts.models import Post
+from user.api.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    category = CategorySerializer()
     class Meta:
         model = Post
         fields = ['title', 'content', 'slug', 'miniature', 'created_at', 'published', 'user', 'category']
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
 from posts.api.serializers import PostSerializer
+from posts.api.permissions import IsAdminOrReadOnly
 
 
 class PostApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.filter(published=True)
-    lookup_field = ["slug"]
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
 
 
